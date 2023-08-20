9c9e3a67c1c03f2ef299635a1aa56bd9381982e4
glbondoc <genreininja@gmail.com>
2023-08-19 23:24:55 -0700

    "Finding the largest among the 3 numbers given"

---
+++ FindLargest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FindLargest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter the third number: ");
+        double num3 = scanner.nextDouble();
+
+        scanner.close();
+
+        double largest;
+
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+
+        System.out.println("with the three given numbers: " + largest + " is the largest one");
+    }
+}
+
